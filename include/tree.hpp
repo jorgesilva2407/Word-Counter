64e02743ae35fa2ba846fcb398158f746dde51a6
Jorge Augusto <jorge.lima2407@gmail.com>
2022-06-26 19:33:04 -0300

    métodos das classes relacionadas a árvores foram documentados, removido o método insert da classe Node e alterado o método insert da classe BST

--- include/tree.hpp
+++ include/tree.hpp
@@ -15,7 +15,6 @@ namespace Tree{
         public:
             Node(std::string word, Node* right, Node* left);
             Node(std::string word) : Node(word, nullptr, nullptr){}
-            void insert(std::string word);
             ~Node();
     };
 
--- src/tree.cpp
+++ src/tree.cpp
@@ -1,43 +1,59 @@
 #include "tree.hpp"
 
-Tree::Node::Node(std::string word, Node* right, Node* left){
+Tree::Node::Node(std::string word, Node* right, Node* left)
+// Descrição: método construtor da classe Node
+// Entrada: recebe uma string, e ponteiros para dois outros nodes
+// Saída: nenhuma
+{
     this->value = new wordCounter::wordCoutner(word);
     this->right = right;
     this->left = left;
 }
 
-void Tree::Node::insert(std::string word){
-    if(value->word == word){
-        value->counter++;
-        return;
-    } else if(value->word > word){
-        if(right == nullptr){
-            right = new Node(word);
-            return;
-        } else {
-            right->insert(word);
-        }
-    } else {
-        if(left == nullptr){
-            left = new Node(word);
-            return;
-        } else {
-            left->insert(word);
-        }
-    }
-}
-
-Tree::Node::~Node(){
+Tree::Node::~Node()
+// Descrição: método destrutor da classe node
+// Entrada: nenhuma
+// Saída: nenhuma
+{
     this->value = nullptr;
     this->right = nullptr;
     this->left = nullptr;
 }
 
-Tree::BST::BST(){
-    this->nElementos = 0;
+Tree::BST::BST()
+// Descrição: método construtor da classe BST
+// Entrada: nenhuma
+// Saída: nenhuma
+{
+    this->nElements = 0;
     this->root = nullptr;
 }
 
-void Tree::BST::insert(std::string word){
-    root->insert(word);    
+void Tree::BST::insert(std::string word)
+// Descrição: insere um elemento na árvore
+// Entrada: uma string
+// Saída: nenhuma
+{
+    Node* current = root;
+    while(current != nullptr){
+        if(current->value->word == word){
+            current->value->counter++;
+        } else if(current->value->word > word){
+            if(current->left == nullptr){
+                current->left = new Node(word);
+                nElements++;
+                return;
+            } else {
+                current = current->left;
+            }
+        } else {
+            if(current->right == nullptr){
+                current->right = new Node(word);
+                nElements++;
+                return;
+            } else {
+                current = current->right;
+            }
+        }
+    }
 }
